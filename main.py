259547d8a55cc6ab3e67caa05b270eff9e1afb74
MarioSeattle <marioseattle@live.com>
2017-06-04 16:53:07 -0700

    Setting up form on .py file

--- main.py
+++ main.py
@@ -3,8 +3,29 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+
+form = """ 
+<!doctype html>
+<html>
+
+<body>
+
+<form>
+    <label for="first-name">First Name:</label>
+    <input id="first-name" type="test" name="first_name"/>
+    <input type="submit"/>
+</form>
+
+</body>
+
+</hmtl>
+
+"""
+
+
 @app.route("/")
 def index():
-    return "Hello Mario!!!!!"
+    return form
+    
 
 app.run()
