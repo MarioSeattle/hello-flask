1a6b6f024e2ecfc43d5a9050c94765cf820ed13c
MarioSeattle <marioseattle@live.com>
2017-06-04 17:52:31 -0700

    Methods-Get-Post, Handlers, sending, action and method routes interaction.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -10,7 +10,7 @@ form = """
 
 <body>
 
-<form>
+<form action="/hello" method="post">
     <label for="first-name">First Name:</label>
     <input id="first-name" type="test" name="first_name"/>
     <input type="submit"/>
@@ -26,6 +26,13 @@ form = """
 @app.route("/")
 def index():
     return form
-    
 
+#handler to catch the inputand send the data to server /hello route
+@app.route("/hello", methods=['POST'])
+def hello():
+#the html will display first_name + greeting message
+#http://127.0.0.1:5000/hello?first_name=Mario
+    first_name = request.form['first_name'] 
+    return '<h1>Hello, ' + first_name + '</h1>'
+        
 app.run()
