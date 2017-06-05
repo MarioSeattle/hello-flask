dc0bc113507c35fe26d3ddb6a38d524437f9b225
MarioSeattle <marioseattle@live.com>
2017-06-05 11:36:22 -0700

    Examples using string.format to create HTML snippes

--- format.py
+++ format.py
@@ -4,21 +4,36 @@
 #Showing the ways to insert ingo using the string method
 #place holder that will insert the value 0
 #We can use name place holder too <title>tittle</title> and call it instead of {1}
+# Using indexed placeholders
 
 markup = """
 <!doctype html>
 <html>
     <head>
         <title>{0}</title>
-
     </head>
     <body>
-        <h1>{1}</h1>
-
+        <h1>{0}</h1>
     </body>
-
 </html>
 """
-#within the format method we can give value to be inserted in the place holder str
-markup = markup.format('My Page Title', 'Page Heading')
+
+markup = markup.format('My Page Title', 'My Page Heading')
+print(markup)
+
+# Using named placeholders
+
+markup = """
+<!doctype html>
+<html>
+    <head>
+        <title>{title}</title>
+    </head>
+    <body>
+        <h1>{heading}</h1>
+    </body>
+</html>
+"""
+
+markup = markup.format(title='My Page Title', heading='My Page Heading')
 print(markup)
