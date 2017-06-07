549d6049c7de1d3235956b1a011caad99939b187
MarioSeattle <marioseattle@live.com>
2017-06-07 11:42:52 -0700

    template integration

--- main.py
+++ main.py
@@ -3,36 +3,28 @@ from flask import Flask, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-
-form = """ 
+form = """
 <!doctype html>
 <html>
-
-<body>
-
-<form action="/hello" method="post">
-    <label for="first-name">First Name:</label>
-    <input id="first-name" type="test" name="first_name"/>
-    <input type="submit"/>
-</form>
-
-</body>
-
-</hmtl>
-
+    <body>
+        <form action="/hello" method="post">
+            <label for="first-name">First Name:</label>
+            <input id="first-name" type="text" name="first_name" />
+            <input type="submit" />
+        </form>
+    </body>
+</html>
 """
 
 @app.route("/")
 def index():
     return form
 
-#handler to catch the inputand send the data to server /hello route
 @app.route("/hello", methods=['POST'])
 def hello():
-#the html will display first_name + greeting message
-#http://127.0.0.1:5000/hello?first_name=Mario
-    first_name = request.form['first_name'] 
+    first_name = request.form['first_name']
     return '<h1>Hello, ' + first_name + '</h1>'
+
 
 time_form = """
     <style>
@@ -51,14 +43,12 @@ time_form = """
         <input type="submit" value="Validate" />
     </form>
     """
-    #places holders {minutes_error}
-    #empty strings will be place on def display_time_format
-    #setup a route
-    #def = handler
-    #@app.route('/validate-time', methods=['POST']) AVAILABLE URL
+
 @app.route('/validate-time')
 def display_time_form():
-    return time_form.format(hours='', hours_error='',minutes='', minutes_error='')
+    return time_form.format(hours='', hours_error='',
+        minutes='', minutes_error='')
+
 
 def is_integer(num):
     try:
@@ -101,7 +91,6 @@ def validate_time():
             minutes_error=minutes_error,
             hours=hours,
             minutes=minutes)
-
 
 
 app.run()
