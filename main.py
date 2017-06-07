fd31d2d971262465ecffcf3b5321592aaaed6dfd
MarioSeattle <marioseattle@live.com>
2017-06-07 11:35:44 -0700

    Using def is_integer to catch errors, variables hold the error messages, ifelse loop checks for formats

--- main.py
+++ main.py
@@ -76,6 +76,32 @@ def validate_time():
     hours_error = ''
     minutes_error = ''
 
+    if not is_integer(hours):
+        hours_error = 'Not a valid integer'
+        hours = ''
+    else:
+        hours = int(hours)
+        if hours > 23 or hours < 0:
+            hours_error = 'Hour value out of range (0-23)'
+            hours = ''
+
+    if not is_integer(minutes):
+        minutes_error = 'Not a valid integer'
+        minutes = ''
+    else:
+        minutes = int(minutes)
+        if minutes > 59 or minutes < 0:
+            minutes_error = 'Minutes value out of range (0-59)'
+            minutes = ''
+
+    if not minutes_error and not hours_error:
+        return "Success!"
+    else:
+        return time_form.format(hours_error=hours_error,
+            minutes_error=minutes_error,
+            hours=hours,
+            minutes=minutes)
+
 
 
 app.run()
