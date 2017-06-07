972b56325e32799b37642aaecdf34d3e66a62e44
MarioSeattle <marioseattle@live.com>
2017-06-07 11:25:55 -0700

    Set up request handler form (def display_time_form), set up route (app.route), get the value of fields importing request, handler to validate if input is an interger with validation function

--- main.py
+++ main.py
@@ -60,6 +60,13 @@ time_form = """
 def display_time_form():
     return time_form.format(hours='', hours_error='',minutes='', minutes_error='')
 
+def is_integer(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+
 @app.route('/validate-time', methods=['POST'])
 def validate_time():
 
@@ -69,31 +76,6 @@ def validate_time():
     hours_error = ''
     minutes_error = ''
 
-    if not is_integer(hours):
-        hours_error = 'Not a valid integer'
-        hours = ''
-    else:
-        hours = int(hours)
-        if hours > 23 or hours < 0:
-            hours_error = 'Hour value out of range (0-23)'
-            hours = ''
-
-    if not is_integer(minutes):
-        minutes_error = 'Not a valid integer'
-        minutes = ''
-    else:
-        minutes = int(minutes)
-        if minutes > 59 or minutes < 0:
-            minutes_error = 'Minutes value out of range (0-59)'
-            minutes = ''
-
-    if not minutes_error and not hours_error:
-        return "Success!"
-    else:
-        return time_form.format(hours_error=hours_error,
-            minutes_error=minutes_error,
-            hours=hours,
-            minutes=minutes)
 
 
 app.run()
